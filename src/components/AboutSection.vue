49758cc8a39070a73674a41d232fa8b11069321e
jfcosm <francisco.carle@gmail.com>
2024-12-09 19:28:02 -0300

    Primer commit: Bienvenidos a mi sitio web oficial

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="about">
+    <h1>About Me</h1>
+    <p>
+      Hi, I'm Francisco Carle, a Senior Scrum Master and Web Developer with extensive experience in Agile methodologies and project leadership. I specialize in crafting efficient solutions using Python and Django, among other technologies.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AboutSection",
+};
+</script>
+
+<style scoped>
+.about {
+  padding: 40px;
+  text-align: center;
+}
+
+.about h1 {
+  color: #4CAF50;
+}
+
+.about p {
+  margin: 10px 0;
+}
+</style>
