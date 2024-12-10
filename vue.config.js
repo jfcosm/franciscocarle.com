fcf8f753164a737293be439fd9ce4f7893b6b83b
jfcosm <francisco.carle@gmail.com>
2024-12-10 17:53:59 -0300

    Noveno commit: Intentanto que la cuestión funcione ajustando el package.json apuntando a main, no a gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/franciscocarle.com/' // Reemplaza con el nombre de tu repositorio
-    : '/'
-};
+  publicPath: '/'
+}
